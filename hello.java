0d2139c1ce2fa5fba1953a11c1869f031cc6e112
= <=>
2020-03-14 10:44:42 +0800

    hello

---
+++ hello.java
@@ -0,0 +1,8 @@
+public class hello{
+		public static void main(String[] args){
+			System.out.println("Say 0")
+			System.out.println("Say hello0")
+			System.out.println("Say 1")
+			System.out.println("Say hello1")
+		}
+}
