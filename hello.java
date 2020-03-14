881dfe49a0228c98b4e55858a2ed7e9215c11c2b
= <=>
2020-03-14 11:33:53 +0800

    other luan

--- hello.java
+++ hello.java
@@ -1,6 +1,10 @@
 public class hello{
 		public static void main(String[] args){
-			System.out.println("Say other hello")
+						System.out.println("Say other 1");
+			System.out.println("Say other hello");
+			System.out.println("Say other 2");
+;
+			System.out.println("Say other 3");
 
 		}
 }
