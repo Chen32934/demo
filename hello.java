2e5b00f968fbd8cfd957cc978b2521259644f7be
= <=>
2020-03-14 11:39:49 +0800

    demo submit

--- hello.java
+++ hello.java
@@ -1,8 +1,8 @@
 public class hello{
 		public static void main(String[] args){
-			System.out.println("Say 0")
-			System.out.println("Say hello0")
-			System.out.println("Say 1")
-			System.out.println("Say hello1")
+			System.out.println("Say 234r5");
+			System.out.println("Say heldfgflo0");
+			System.out.println("Say gxxcvbhj");
+			System.out.println("Say hedfghjkllo1");
 		}
 }
