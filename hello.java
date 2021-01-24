01930a764349f787c36b65ddb45c544198c180d6
dw <cdw32934@gmail.com>
2021-01-24 19:16:40 +0800

    add helloworld.java

--- hello.java
+++ hello.java
@@ -1,14 +1,11 @@
 public class hello{
 		public static void main(String[] args){
-						System.out.println("Say other 1");
-			System.out.println("Say other hello");
-			System.out.println("Say other 2");
-;
-			System.out.println("Say other 3");
+			System.out.println("Say other  1");
+			System.out.println("Say other  hello");
+			System.out.println("Say other  2");
+		    System.out.println("Say other  hello");
+			System.out.println("Say other  3");
+		    System.out.println("Say other  hello");
 
-			System.out.println("Say 234r5");
-			System.out.println("Say heldfgflo0");
-			System.out.println("Say gxxcvbhj");
-			System.out.println("Say hedfghjkllo1");
 		}
 }
